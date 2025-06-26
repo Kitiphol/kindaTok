1d38e82e37b6ab159010f21233324ba5a1def687
Game <Kitiphol@gmail.com>
2025-06-26 21:22:11 +0700

    fix: frontend for k8s #3

--- frontend/toktik_frontend/next.config.ts
+++ frontend/toktik_frontend/next.config.ts
@@ -1,7 +1,18 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // ✅ Required for Docker deployment
+  reactStrictMode: true, // Optional but recommended
 };
 
 export default nextConfig;
+
