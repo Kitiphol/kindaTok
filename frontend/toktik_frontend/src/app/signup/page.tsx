854aec50ebea874504bc9f7f424e21f05862e5ae
Game <Kitiphol@gmail.com>
2025-06-26 23:48:24 +0700

    minor fix on frontend sign up page

--- frontend/toktik_frontend/src/app/signup/page.tsx
+++ frontend/toktik_frontend/src/app/signup/page.tsx
@@ -125,8 +125,8 @@ export default function SignupPage() {
     e.preventDefault();
 
     try {
-      const res = await fetch('/api/auth/register', {
       // const res = await fetch('http://localhost:8080/api/auth/register', {
+      const res = await fetch('/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
